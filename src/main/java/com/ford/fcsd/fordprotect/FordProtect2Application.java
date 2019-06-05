514ecf0249b5eef24822079a719dddf6ef72e7ed
Jorge Rasuck <jrasuck@ford.com>
2019-06-05 17:47:54 -0300

    terceiro commit

--- src/main/java/com/ford/fcsd/fordprotect/FordProtect2Application.java
+++ src/main/java/com/ford/fcsd/fordprotect/FordProtect2Application.java
@@ -1,35 +1,15 @@
 package com.ford.fcsd.fordprotect;
 
-import java.time.LocalDate;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.ford.fcsd.fordprotect.dto.FP2InvoiceDTO;
-import com.ford.fcsd.fordprotect.producer.FordProtectInvoiceProducer;
-
+@EnableScheduling
 @SpringBootApplication
-public class FordProtect2Application implements CommandLineRunner {
-
-	@Autowired
-	private FordProtectInvoiceProducer producer;
+public class FordProtect2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FordProtect2Application.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		for (int i = 0; i < 5; i++) {
-
-			FP2InvoiceDTO invoice = new FP2InvoiceDTO(
-					i, "Invoice " + i, LocalDate.now());
-			producer.sendMessage(invoice);
-		}
-
 	}
 
 }
---
+++ src/main/java/com/ford/fcsd/fordprotect/job/FP2InvoiceJob.java
@@ -0,0 +1,27 @@
+package com.ford.fcsd.fordprotect.job;
+
+import java.time.LocalDate;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.ford.fcsd.fordprotect.dto.FP2InvoiceDTO;
+import com.ford.fcsd.fordprotect.producer.FordProtectInvoiceProducer;
+
+@Component
+public class FP2InvoiceJob {
+
+	private FordProtectInvoiceProducer producer;
+	
+	@Scheduled(cron="0 * * * * *")
+	public void doSelectInvoices() {
+		
+		for (int i = 0; i < 10; i++) {
+			FP2InvoiceDTO invoice = 
+					new FP2InvoiceDTO(i, "Invoice " + i, LocalDate.now());
+			producer.sendMessage(invoice);
+		}
+		
+	}
+	
+}
